c78ee0ef7ba797f7000276dc2da64a9ea9fe6234
yoyobazzi3 <yoyobazzi3@gmail.com>
2025-01-17 09:52:20 -0800

    neetcofe

---
+++ Arrays&Hashing/twoSum.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const map = new Map();
+        for(let i = 0; i < nums.length; i ++){
+            compliment = target - nums[i];
+            if(map.has(compliment)){
+                return [map.get(compliment),i]
+            }
+            map.set(nums[i],i)
+        }
+        return [];
+    }
+}
--- Arrays&Hashing/validAnagram.js
+++ Arrays&Hashing/validAnagram.js
@@ -5,11 +5,20 @@ class Solution {
      * @return {boolean}
      */
     isAnagram(s, t) {
-     if(s.length !== t.length){
+    if(s.length !== t.length){
         return false;
      }
      const frequency1 = {}
      const frequency2 = {}
-     
+     for (let char of s){
+        frequency1[char] = ((frequency1[char] || 0) + 1)
+        frequency2[char] = ((frequency2[char] || 0) + 1)
+     }
+     for(let keys of frequency1){
+        if(frequency1[keys] !== frequency2[keys]){
+            return false;
+        }
+     }
+     return true;
     }
 }
