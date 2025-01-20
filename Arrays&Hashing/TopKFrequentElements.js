093aa88f1762a818a4c2b684a2f5baadc5ea7417
yoyobazzi3 <yoyobazzi3@gmail.com>
2025-01-19 17:23:48 -0800

    groupAnagram

---
+++ Arrays&Hashing/TopKFrequentElements.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        k = {}
+
+        for(i of nums){
+            
+        }
+    }
+}
---
+++ Arrays&Hashing/groupAnagrams.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+       const res = {};
+       for(let s of strs){
+        const count = new Array(26).fill(0);
+        for (let c of s){
+            count[c.charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+        }
+        const key = count.join(',')
+
+        if(!res[key]){
+            res[key] = [];
+        }
+        res[key].push(s);
+       }
+       return Object.values(res)
+    }
+}
