42bdc7a397a3e44fb285843a32acc3e60eef3dc2
yoyobazzi3 <yoyobazzi3@gmail.com>
2025-01-19 17:27:53 -0800

    groupAnagram

--- Arrays&Hashing/groupAnagrams.js
+++ Arrays&Hashing/groupAnagrams.js
@@ -4,19 +4,18 @@ class Solution {
      * @return {string[][]}
      */
     groupAnagrams(strs) {
-       const res = {};
-       for(let s of strs){
-        const count = new Array(26).fill(0);
-        for (let c of s){
-            count[c.charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+        const res = {};
+        for(let s of strs){
+            const count = new Array(26).fill(0);
+            for(let c of s){
+                count[c.charCodeAt(0) - 'a'.charCodeAt(0)] +=1;
+            }
+            const key = count.join(',');
+            (!res[key]){
+                res[key] = []
+            }
+            res[key].push(s);
         }
-        const key = count.join(',')
-
-        if(!res[key]){
-            res[key] = [];
-        }
-        res[key].push(s);
-       }
-       return Object.values(res)
+        return Object.values(res);
     }
 }
