1c529bed0bddbd1a2beafd823de167288470f6ec
yoyobazzi3 <yoyobazzi3@gmail.com>
2025-01-31 13:19:58 -0800

    neetcode

---
+++ Arrays&Hashing/isPalinfromeNUmber.js
@@ -0,0 +1,47 @@
+class Solution{
+   
+    isPalindrome(x) {
+        let str = x.toString();
+        
+        let r = str.length-1;
+        let l = 0;
+        if(str[0] === '-'){
+            l = 1;
+        }else{
+            l = 0;
+        }
+
+
+         while(l<r){
+          if(str[l] !== str[r]){
+            return false;
+            }
+            l++;
+            r--;
+        }
+            return true;
+    };
+
+     range(lower, upper){
+        let res=[];
+        const palindrome = new Solution();
+        for(let i = lower; i<=upper ; i++){
+            if(palindrome.isPalindrome(i)){
+                res.push(i);
+            }
+        }
+
+        if(!res.length){
+            return [];
+        }else{
+            return res;
+        }
+        
+
+    }
+}
+
+const solution = new Solution();
+console.log(solution.isPalindrome(-121));
+const result = solution.range(121,200);
+console.log(result);
---
+++ Binary Search/Search2DMatrix.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        let ROWS = matrix.length,
+            COLS = matrix[0].length;
+
+        let l = 0;
+        let r = ROWS * COLS - 1;
+        while(l<=r){
+
+        }
+    }
+}
